77e4d33d422d099352e076a2bb7e9514dc008034
derminnn <dharminpanchal24@gmail.com>
2026-03-11 15:51:19 +0530

    Add malloc example for dynamic memory allocation
    Implement basic memory allocation using malloc and free.

---
+++ DAY -6 -heap memory /01_malloc_basic/malloc.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.c>
+
+int main()
+{
+  int *ptr;
+  ptr = (int*) malloc(5*sizeof(int));
+  if(ptr == NULL) 
+{
+    printf("memory allocaton failed\n");
+    return 1;
+}
+ptr[0] = 10;
+ptr[1] = 20;
+ptr[2] = 30;
+ptr[3] = 40;
+ptr[4] = 50;
+
+for(int i=0;i<5;i++)
+{
+   printf("value : %d  |  Address : %p\n",ptr[i],&ptr[i]);
+}
+free(ptr);
+ptr = NULL;
+return 0;
