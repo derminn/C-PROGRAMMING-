7e9c6427b1d39c0250220d90f1d3f9c687e4e72b
derminnn <dharminpanchal24@gmail.com>
2026-03-11 16:05:57 +0530

    Update memory_leak_demo.c

--- DAY-06-Heap_memory/02_memory_leak/memory_leak_demo.c
+++ DAY-06-Heap_memory/02_memory_leak/memory_leak_demo.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int iterations = 100;
+    int bytes_per_alloc = 100;
+    int total_bytes = 0;
+
+    for(int i = 1; i <= iterations; i++)
+    {
+        char *ptr = (char*) malloc(bytes_per_alloc);
+
+        if(ptr == NULL)
+        {
+            printf("Memory allocation failed\n");
+            return 1;
+        }
+
+        total_bytes += bytes_per_alloc;
+
+        printf("Allocation #%d | Total bytes allocated: %d\n", i, total_bytes);
+
+        /* INTENTIONALLY NOT freeing memory */
+    }
+
+    printf("Program finished (memory leak occurred)\n");
+
+    return 0;
+}
