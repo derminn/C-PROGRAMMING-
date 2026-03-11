fb45639cf938495a26be95fdffc5e84dba3768ec
derminnn <dharminpanchal24@gmail.com>
2026-03-11 16:06:38 +0530

    Update fixed_memory_leak.c

--- DAY-06-Heap_memory/02_memory_leak/fixed_memory_leak.c
+++ DAY-06-Heap_memory/02_memory_leak/fixed_memory_leak.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int iterations = 100;
+    int bytes_per_alloc = 100;
+    int total_bytes = 0;
+
+    for(int i = 1; i <= iterations; i++)
+    {
+        char *ptr = (char*) malloc(bytes_per_alloc);
+
+        if(ptr == NULL)
+        {
+            printf("Memory allocation failed\n");
+            return 1;
+        }
+
+        total_bytes += bytes_per_alloc;
+
+        printf("Allocation #%d | Total bytes allocated: %d\n", i, total_bytes);
+
+        free(ptr);
+        ptr = NULL;
+    }
+
+    printf("Program finished (no memory leak)\n");
+
+    return 0;
+}
