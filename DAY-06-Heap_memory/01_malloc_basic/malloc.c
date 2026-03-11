e5f1090df9af515f4f1a86fe85ab5a4995c044a8
derminnn <dharminpanchal24@gmail.com>
2026-03-11 15:59:28 +0530

    Update malloc.c

--- DAY-06-Heap_memory/01_malloc_basic/malloc.c
+++ DAY-06-Heap_memory/01_malloc_basic/malloc.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int *ptr;
+
+    ptr = (int*) malloc(5 * sizeof(int));
+
+    if(ptr == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    ptr[0] = 10;
+    ptr[1] = 20;
+    ptr[2] = 30;
+    ptr[3] = 40;
+    ptr[4] = 50;
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Value: %d | Address: %p\n", ptr[i], &ptr[i]);
+    }
+
+    free(ptr);
+    ptr = NULL;
+
+    return 0;
+}
