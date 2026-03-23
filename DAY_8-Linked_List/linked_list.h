3fa834b7e7992064212bc01ca100ca9a37b777f5
derminnn <dharminpanchal24@gmail.com>
2026-03-23 16:18:33 +0530

    Update linked_list.h

--- DAY_8-Linked_List/linked_list.h
+++ DAY_8-Linked_List/linked_list.h
@@ -1,1 +1,19 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
 
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+Node *createNode(int value);
+void  append(Node **head, int value);
+void  prepend(Node **head, int value);
+void  printList(Node *head);
+int   search(Node *head, int target);
+void  deleteNode(Node **head, int target);
+void  reverse(Node **head);
+int   countNodes(Node *head);
+void  freeList(Node *head);
+
+#endif
